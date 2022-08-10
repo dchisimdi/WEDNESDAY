dd836ec534eba89252a78c45b38b56496bba0188
ekubechisimdi <chisimdiekube61@gmail.com>
2022-08-10 11:12:18 +0100

    my folder

---
+++ Home.ts
@@ -0,0 +1,7 @@
+let firstName: string ='simdi'
+let age: number =13;
+let lastname: string ='doe';
+console.warn ('last name variable is in camel case', lastname);
+if (typeof(age) !== 'number'){
+    console.error('age is not a number')
+}
